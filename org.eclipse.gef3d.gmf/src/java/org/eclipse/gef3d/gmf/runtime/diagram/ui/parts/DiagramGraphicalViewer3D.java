55671dae8e1f224f15ee50298410429ccf01338d
Jens von Pilgrim <developer@jevopi.de>
2009-04-26 16:08:17 +0000

    fixed some copyright issues (see https://dev.eclipse.org/ipzilla/show_bug.cgi?id=2806 for details)

--- org.eclipse.gef3d.gmf/src/java/org/eclipse/gef3d/gmf/runtime/diagram/ui/parts/DiagramGraphicalViewer3D.java
+++ org.eclipse.gef3d.gmf/src/java/org/eclipse/gef3d/gmf/runtime/diagram/ui/parts/DiagramGraphicalViewer3D.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2009 Jens von Pilgrim and others.
+ * Copyright (c) 2009 Kristian Duske and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Jens von Pilgrim - initial API and implementation
+ *    Kristian Duske - initial API and implementation
  ******************************************************************************/
 package org.eclipse.gef3d.gmf.runtime.diagram.ui.parts;
 
