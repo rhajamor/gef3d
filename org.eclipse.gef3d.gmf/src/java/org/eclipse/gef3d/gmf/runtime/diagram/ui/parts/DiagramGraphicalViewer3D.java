ef34519b191e45c196fd78ec98a04a039677218b
Jens von Pilgrim <developer@jevopi.de>
2009-08-05 11:17:59 +0000

    - introduced GraphicalViewer3DHelper, which is used in Graphical3DViewer implementations for 3D specific tasks - DiagramGraphicalViewer3D (GMF) and GraphicalViewer3D (GEF3D) are both using the before mentioned helper - Ecore editor 3D replaces the RubberbandSelectionTracker with a standard tracker w/o casts problems

--- org.eclipse.gef3d.gmf/src/java/org/eclipse/gef3d/gmf/runtime/diagram/ui/parts/DiagramGraphicalViewer3D.java
+++ org.eclipse.gef3d.gmf/src/java/org/eclipse/gef3d/gmf/runtime/diagram/ui/parts/DiagramGraphicalViewer3D.java
@@ -34,10 +34,12 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Handle;
 import org.eclipse.gef.LayerConstants;
+import org.eclipse.gef.EditPartViewer.Conditional;
 import org.eclipse.gef.editparts.LayerManager;
 import org.eclipse.gef3d.gmf.runtime.core.service.IProviderAcceptorProvider;
 import org.eclipse.gef3d.gmf.runtime.core.service.ProviderAcceptor;
 import org.eclipse.gef3d.ui.parts.GraphicalViewer3D;
+import org.eclipse.gef3d.ui.parts.GraphicalViewer3DHelper;
 import org.eclipse.gmf.runtime.common.ui.services.editor.IEditorProvider;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramGraphicalViewer;
 import org.eclipse.swt.SWT;
@@ -56,13 +58,18 @@ public class DiagramGraphicalViewer3D extends DiagramGraphicalViewer implements
 		GraphicalViewer3D, IProviderAcceptorProvider {
 
 	/**
+	 * Helper, implementing most methods needed in this implementation. The
+	 * methods in this class delegate to the helper methods.
+	 */
+	protected GraphicalViewer3DHelper m_ViewerHelper;
+	
+	/**
 	 * Creates this viewer and adds a {@link ProviderAcceptor} to its
 	 * properties. This provider selector is also attached as viewer to the
 	 * diagram, in order to be accessible via {@link IEditorProvider}, see
 	 * {@link IProviderSelector} for details.
 	 */
 	public DiagramGraphicalViewer3D() {
-
 		this(new ProviderAcceptor(true));
 
 	}
@@ -74,7 +81,9 @@ public class DiagramGraphicalViewer3D extends DiagramGraphicalViewer implements
 	 * @param providerAcceptor
 	 */
 	public DiagramGraphicalViewer3D(ProviderAcceptor providerAcceptor) {
-
+		if (m_ViewerHelper==null) {
+			m_ViewerHelper = new GraphicalViewer3DHelper(this);
+		}
 		setProperty(ProviderAcceptor.PROVIDER_ACCEPTOR_PROPERTY_KEY,
 			providerAcceptor);
 	}
@@ -100,13 +109,8 @@ public class DiagramGraphicalViewer3D extends DiagramGraphicalViewer implements
 	 * @return the GL canvas
 	 */
 	public Control createControl3D(Composite i_composite) {
-
-		GLCanvas canvas =
-			Draw3DCanvas.createCanvas(i_composite, SWT.NONE,
-				getLightweightSystem3D());
-
-		setControl(canvas);
-		return getControl();
+		return m_ViewerHelper.createControl3D(i_composite,
+			getLightweightSystem3D());
 	}
 
 	/**
@@ -116,8 +120,10 @@ public class DiagramGraphicalViewer3D extends DiagramGraphicalViewer implements
 	 */
 	@Override
 	protected LightweightSystem createLightweightSystem() {
-
-		return new LightweightSystem3D();
+		if (m_ViewerHelper==null) {
+			m_ViewerHelper = new GraphicalViewer3DHelper(this);
+		}
+		return m_ViewerHelper.createLightweightSystem();
 	}
 
 	/**
@@ -135,136 +141,25 @@ public class DiagramGraphicalViewer3D extends DiagramGraphicalViewer implements
 
 	/**
 	 * {@inheritDoc}
-	 * <p>
-	 * This method was copied and subsequently modified.
-	 * </p>
 	 * 
-	 * @author hudsonr (original implementation)
-	 * @author Kristian Duske
 	 * @see org.eclipse.gef.ui.parts.GraphicalViewerImpl#findHandleAt(org.eclipse.draw2d.geometry.Point)
 	 */
 	@Override
-	public Handle findHandleAt(Point i_mLocation) {
-
-		LayerManager layermanager =
-			(LayerManager) getEditPartRegistry().get(LayerManager.ID);
-
-		if (layermanager == null)
-			return null;
-
-		Vector3f eye = Draw3DCache.getVector3f();
-		Vector3f wLocation = Draw3DCache.getVector3f();
-		Vector3f direction = Draw3DCache.getVector3f();
-		Point sLocation = Draw3DCache.getPoint();
-		try {
-			List<IFigure> ignore = new ArrayList<IFigure>(3);
-			ignore.add(layermanager.getLayer(LayerConstants.PRIMARY_LAYER));
-			ignore.add(layermanager.getLayer(LayerConstants.CONNECTION_LAYER));
-			ignore.add(layermanager.getLayer(LayerConstants.FEEDBACK_LAYER));
-			TreeSearch search = new ExclusionSearch(ignore);
-
-			LightweightSystem3D lws = getLightweightSystem3D();
-			Picker picker = lws.getPicker();
-
-			Hit hit = picker.getHit(i_mLocation.x, i_mLocation.y, search);
-			if (hit == null)
-				return null;
-
-			IFigure3D figure3D = hit.getFigure();
-			if (figure3D instanceof Handle)
-				return (Handle) figure3D;
-
-			// keep searching on the surface
-			lws.getCamera().getPosition(eye);
-			lws.getCamera().unProject(i_mLocation.x, i_mLocation.y, 0, null,
-				wLocation);
-
-			ISurface surface = figure3D.getSurface();
-
-			Math3D.getRayDirection(eye, wLocation, direction);
-			surface.getSurfaceLocation2D(eye, direction, sLocation);
-
-			IFigure figure2D =
-				figure3D.findFigureAt(sLocation.x, sLocation.y, search);
-
-			if (figure2D instanceof Handle)
-				return (Handle) figure2D;
-
-			return null;
-		} finally {
-			Draw3DCache.returnVector3f(eye, wLocation, direction);
-			Draw3DCache.returnPoint(sLocation);
-		}
+	public Handle findHandleAt(Point i_sLocation) {
+		return m_ViewerHelper.findHandleAt(i_sLocation);
 	}
 
 	/**
 	 * {@inheritDoc}
-	 * <p>
-	 * This method was copied and subsequently modified.
-	 * </p>
 	 * 
-	 * @author hudsonr (original implementation)
-	 * @author Kristian Duske
 	 * @see org.eclipse.gef.ui.parts.GraphicalViewerImpl#findObjectAtExcluding(org.eclipse.draw2d.geometry.Point,
 	 *      java.util.Collection, org.eclipse.gef.EditPartViewer.Conditional)
 	 */
-	@SuppressWarnings("unchecked")
 	@Override
-	public EditPart findObjectAtExcluding(Point i_mLocation,
+	public EditPart findObjectAtExcluding(Point i_sLocation,
 		Collection i_exclude, final Conditional i_condition) {
-
-		class ConditionalTreeSearch extends ExclusionSearch {
-
-			ConditionalTreeSearch(Collection coll) {
-
-				super(coll);
-			}
-
-			@Override
-			public boolean accept(IFigure i_figure) {
-
-				IFigure current = i_figure;
-				EditPart editpart = null;
-				while (editpart == null && current != null) {
-					editpart = (EditPart) getVisualPartMap().get(current);
-					current = current.getParent();
-				}
-
-				return editpart != null
-					&& (i_condition == null || i_condition.evaluate(editpart));
-			}
-		}
-
-		Vector3f rayOrigin = Math3DCache.getVector3f();
-		Vector3f rayDirection = Math3DCache.getVector3f();
-		try {
-
-			LightweightSystem3D lws = getLightweightSystem3D();
-			Picker picker = lws.getPicker();
-
-			TreeSearch search = new ConditionalTreeSearch(i_exclude);
-			Hit hit = picker.getHit(i_mLocation.x, i_mLocation.y, search);
-
-			EditPart part = null;
-			if (hit != null) {
-				IFigure3D figure3D = hit.getFigure();
-				IFigure figure2D =
-					figure3D.findFigureAt(i_mLocation.x, i_mLocation.y, search);
-
-				while (part == null && figure2D != null) {
-					part = (EditPart) getVisualPartMap().get(figure2D);
-					figure2D = figure2D.getParent();
-				}
-			}
-
-			if (part == null)
-				return getContents();
-
-			return part;
-		} finally {
-			Math3DCache.returnVector3f(rayOrigin, rayDirection);
-			Math3DCache.returnVector3f(rayDirection);
-		}
+		return m_ViewerHelper.findObjectAtExcluding(i_sLocation, i_exclude,
+			i_condition);
 	}
 
 	/**
