8e00003c015934c3a1406fc3569f982291ef2929
Jens von Pilgrim <developer@jevopi.de>
2011-06-28 10:48:28 +0000

    x3d is no longer maintained

---
+++ org.eclipse.draw3d.x3d/src/java/org/eclipse/draw3d/graphics3d/x3d/draw/X3DDrawTargetFactory.java
@@ -0,0 +1,141 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Jens von Pilgrim and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Matthias Thiele - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.draw3d.graphics3d.x3d.draw;
+
+import org.eclipse.draw3d.graphics3d.Graphics3DException;
+
+/**
+ * This factory creates draw targets.
+ * 
+ * @author Matthias Thiele
+ * @version $Revision$
+ * @since Dec 15, 2008
+ */
+public class X3DDrawTargetFactory {
+
+	/**
+	 * A line graphics primitive which contains exactly two vertices.
+	 */
+	public static final int DRAW_TARGET_LINE = 1;
+
+	/**
+	 * A polygon primitive which may contain any number of vertices. Every two
+	 * vertices define one edge of the polygon.
+	 */
+	public static final int DRAW_TARGET_POLYGON = 2;
+
+	/**
+	 * Similar to {@link #DRAW_TARGET_POLYGON} which the difference that the
+	 * last and the first vertex define a closing edge.
+	 */
+	public static final int DRAW_TARGET_POLYGON_LOOP = 3;
+
+	/**
+	 * A quad graphics primitive, which may also get a texture assigned or
+	 * filled with any diffuse color.
+	 */
+	public static final int DRAW_TARGET_QUAD = 4;
+
+	/**
+	 * A set of individual triangles.
+	 */
+	public static final int DRAW_TARGET_TRIANGLE_SET = 5;
+
+	/**
+	 * A fan of triangles.
+	 */
+	public static final int DRAW_TARGET_TRIANGLE_FAN_SET = 6;
+
+	/**
+	 * A strip of triangles.
+	 */
+	public static final int DRAW_TARGET_TRIANGLE_STRIP_SET = 7;
+
+	/**
+	 * Creates a draw target of the specified type.
+	 * 
+	 * @param i_iType
+	 *            The type of draw target to create.
+	 * @return The created draw target.
+	 */
+	public static X3DDrawTarget createDrawTarget(int i_iType) {
+
+		X3DDrawTarget dt = null;
+
+		switch (i_iType) {
+
+		case DRAW_TARGET_LINE:
+			dt = new X3DLine();
+			break;
+
+		case DRAW_TARGET_QUAD:
+			dt = new X3DQuad();
+			break;
+
+		case DRAW_TARGET_POLYGON:
+			dt = new X3DPolygon(false);
+			break;
+
+		case DRAW_TARGET_POLYGON_LOOP:
+			dt = new X3DPolygon(true);
+			break;
+
+		case DRAW_TARGET_TRIANGLE_SET:
+			dt = new X3DTriangleSet();
+			break;
+			
+		case DRAW_TARGET_TRIANGLE_FAN_SET:
+			dt = new X3DTriangleFanSet();
+			break;
+		
+		case DRAW_TARGET_TRIANGLE_STRIP_SET:
+			dt = new X3DTriangleStripSet();
+			break;
+
+		default:
+			throw new Graphics3DException("Unknown draw target type: "
+					+ i_iType);
+
+		}
+
+		return dt;
+	}
+
+	/**
+	 * This method can be used to create a new instance of an existing draw
+	 * target.
+	 * 
+	 * @param original
+	 *            The original draw target
+	 * @return A new draw target instance of the same type as the original.
+	 */
+	public static X3DDrawTarget createNewInstance(X3DDrawTarget original) {
+		if (original instanceof X3DLine) {
+			return new X3DLine();
+		} else if (original instanceof X3DQuad) {
+			return new X3DQuad();
+		} else if (original instanceof X3DPolygon) {
+			X3DPolygon poly = (X3DPolygon) original;
+			return new X3DPolygon(poly.isLooped());
+		} else if (original instanceof X3DTriangleSet) {
+			return new X3DTriangleSet();
+		} else if (original instanceof X3DTriangleFanSet) {
+			return new X3DTriangleFanSet();
+		} else if (original instanceof X3DTriangleStripSet) {
+			return new X3DTriangleStripSet();
+		} else {
+			throw new Graphics3DException("Unknown original: "
+					+ original.getClass());
+		}
+	}
+
+}
---
+++ org.eclipse.draw3d.x3d/src/java/org/eclipse/draw3d/graphics3d/x3d/lists/package-info.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Jens von Pilgrim and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Matthias Thiele - initial API and implementation
+ ******************************************************************************/
+
+/**
+ * <p>This package provide a method call buffer. This is a very generic mechanism
+ * which addresses the display lists of OpenGL. Method calls can be added to this
+ * buffer and executed afterwards.
+ * </p>
+ * 
+ * @author Matthias Thiele
+ * @version $Revision$
+ * @since 30.01.2009
+ */
+package org.eclipse.draw3d.graphics3d.x3d.lists;
